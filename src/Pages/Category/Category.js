512948dc01afd6c5a42353bfcac8f686ed12f99e
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-10-19 23:41:11 +0600

    auth start

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Newsummerycard from "../Shared/Newsummerycard/Newsummerycard";
 
 const Category = () => {
+  const CategoryNews = useLoaderData();
   return (
     <div>
-      <h2>this is category</h2>
+      <h2>this category has total news {CategoryNews.length}</h2>
+      {CategoryNews.map((news) => (
+        <Newsummerycard key={news._id} news={news}></Newsummerycard>
+      ))}
     </div>
   );
 };
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import Newsummerycard from "../Shared/Newsummerycard/Newsummerycard";
 
 const Home = () => {
+  const AllNews = useLoaderData();
   return (
     <div>
-      <h2>this is Home page</h2>
+      <h2>Dragon News quantity{AllNews.length}</h2>
+      {AllNews.map((news) => (
+        <Newsummerycard key={news._id} news={news}></Newsummerycard>
+      ))}
     </div>
   );
 };
